68798c96fdb3cecc39d549a88e41f8e6ea9cb1ea
longhike <sam.ascheim@gmail.com>
2021-01-28 11:27:59 -0500

    ugly hardcoding

--- views/script.js
+++ views/script.js
@@ -1,5 +1,5 @@
 const userPage = document.getElementById("user-page");
-const userTree = document.getElementById("user-tree");
+// const userTree = document.getElementById("user-tree");
 const userEdit = document.getElementById("user-edit");
 
 const homeButton = document.getElementById("home-button");
@@ -9,21 +9,33 @@ const editButton = document.getElementById("edit-button");
 homeButton.addEventListener("click", (e) => {
     e.preventDefault()
     userPage.style.display = "block"
-    userTree.style.display = "none"
+    // userTree.style.display = "none"
     userEdit.style.display = "none"
 })
 
-treeButton.addEventListener("click", (e) => {
-    e.preventDefault()
-    userPage.style.display = "none"
-    userEdit.style.display = "none"
-    userTree.style.display = "block"
-})
+// treeButton.addEventListener("click", (e) => {
+//     e.preventDefault()
+//     userPage.style.display = "none"
+//     userEdit.style.display = "none"
+//     // userTree.style.display = "block"
+// })
 
 editButton.addEventListener("click", (e) => {
     e.preventDefault()
     userPage.style.display = "none"
-    userTree.style.display = "none"
+    // userTree.style.display = "none"
     userEdit.style.display = "block"
     
 })
+
+document.getElementsByClassName('_col')[0].addEventListener("click", (e) => {
+    e.preventDefault()
+    e.target.style = "data-toggle: popover"
+    e.target.style = "data-placement: right"
+    e.target.style = `data-content: Hannah's fam`
+})
+function handlePopover (div, content) {
+    div.style = "data-toggle: popover"
+    div.style = "data-placement: right"
+    div.style = `data-content: ${content}`
+}
